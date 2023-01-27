0699b6126d8e83f37674e25769dccb94ddb965c3
Emanuel Dobra <ema.dobra@gmail.com>
2023-01-26 21:14:46 -0500

    Created base match game

--- app/src/main/java/comp208/dobra/MainActivity.java
+++ app/src/main/java/comp208/dobra/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Start comment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
